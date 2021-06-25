a8171936504ed6f15bf09a63792c254d593d19b0
Aws-star <75694518+Aws-star@users.noreply.github.com>
2021-06-25 14:42:52 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 console.log("Your index.js file is loaded correctly!");
+/*$("button").hover(function(){
+  $(this).css("background-color", "yellow");
+}); */
+
+$("#button2").hover(function(){
+	$(this).css("background-color","#02498A");
+});
